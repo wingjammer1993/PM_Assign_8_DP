8f4cd5d0e846a0353176e9f823b9d80e264c1006
amruta <hail.radical@gmail.com>
2018-04-14 18:40:13 -0600

    CRP Process graph

---
+++ Task_1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def give_unoccupied_crp_distribution(alpha, total_customers):
+	probability_unoccupied_table = np.zeros(total_customers)
+	for customer in range(1, total_customers):
+		probability_unoccupied_table[customer] = alpha / (customer - 1 + alpha)
+	plt.plot(probability_unoccupied_table[1:])
+	plt.locator_params(axis='x', nbins=20)
+	plt.xlabel("Number of customers")
+	plt.ylabel("Probability of empty table assignment")
+	plt.title("Number of customers in the hotel vs. Probability of empty table assignment with CRP")
+	plt.show()
+
+
+give_unoccupied_crp_distribution(0.5, 500)
+
